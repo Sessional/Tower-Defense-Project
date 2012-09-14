3e02a0cdc9459d6bb789b1b5b0dc26ed93085f67
Sessional <andrew.kruse@blackhole.com>
2012-09-14 14:21:05 -0500

    WaveManager
    Beginning of waves.

---
+++ TowerDefenseGame/TowerDefenseGame/Waves/Wave.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TowerDefenseGame.Monsters;
+
+namespace TowerDefenseGame.Waves
+{
+    public class Wave
+    {
+
+        private int liveMonsters;
+        private int monstersToSpawn;
+        private int monstersSpawned;
+
+        private Monster monsterType;
+
+        public bool IsWaveComplete()
+        {
+            return false;
+        }
+    }
+}
---
+++ TowerDefenseGame/TowerDefenseGame/Waves/WaveManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefenseGame.Waves
+{
+    public class WaveManager
+    {
+
+        private Wave currentWave;
+        private Wave nextWave;
+
+        public Wave GetCurrentWave()
+        {
+            return currentWave;
+        }
+
+        public Wave GetNextWave()
+        {
+            return nextWave;
+        }
+
+        public void ProgressWaves()
+        {
+            currentWave = nextWave;
+
+            GenerateNextWave();
+        }
+
+        public void GenerateNextWave()
+        {
+
+        }
+
+        public bool IsWaveComplete()
+        {
+            return currentWave.IsWaveComplete();
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
